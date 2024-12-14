d2d31219386095979b5ad3433c8be3bb4ad4a893
baptiste gresse <baptiste5gresse@gmail.com>
2024-12-14 09:36:52 +0100

    done

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,7 @@ const nextConfig: NextConfig = {
   redirects: async () => [
     {
       source: '/',
-      destination: '/home',
+      destination: '/fr/home',
       permanent: false,
     }
   ]
