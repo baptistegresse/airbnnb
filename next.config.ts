48e543b4c7308a36bf669a904fe76f528d8d73b4
baptiste gresse <baptiste5gresse@gmail.com>
2024-12-14 09:53:16 +0100

    done

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  redirects: async () => [
-    {
-      source: '/',
-      destination: 'fr/home',
-      permanent: false,
-    }
-  ]
 };
 
 export default nextConfig;
