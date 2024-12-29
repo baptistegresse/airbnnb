02d409a43e91931a762923460b5e656f7fcc9cad
baptiste gresse <baptiste5gresse@gmail.com>
2024-12-29 16:22:58 +0100

    done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,7 @@
 import { redirect } from 'next/navigation'
+import { useCurrentLocale } from '@/locales/client';
 
 export default function Page() {
-  redirect('/home')
+  const locale = useCurrentLocale();
+  redirect(`/${locale}/home`);
 }
