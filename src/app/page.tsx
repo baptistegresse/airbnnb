fc17f3f6adced229a8d93e7be85a195098868904
baptiste gresse <baptiste5gresse@gmail.com>
2024-12-29 16:38:06 +0100

    done

--- src/app/page.tsx
+++
@@ -1,9 +0,0 @@
-'use client'
-
-import { redirect } from 'next/navigation'
-import { useCurrentLocale } from '@/locales/client';
-
-export default function Page() {
-  const locale = useCurrentLocale();
-  redirect(`/${locale}/home`);
-}
--- src/middleware.ts
+++ src/middleware.ts
@@ -1,16 +1,22 @@
 // middleware.ts
 import { createI18nMiddleware } from 'next-international/middleware'
 import { NextRequest } from 'next/server'
- 
+
 const I18nMiddleware = createI18nMiddleware({
   locales: ['en', 'fr'],
   defaultLocale: 'en'
 })
- 
+
 export function middleware(request: NextRequest) {
+  // Redirection de / vers /home
+  if (request.nextUrl.pathname === '/') {
+    return Response.redirect(new URL('/home', request.url))
+  }
+
+  // Gestion de l'internationalisation
   return I18nMiddleware(request)
 }
- 
+
 export const config = {
   matcher: ['/((?!api|static|.*\\..*|_next|favicon.ico|robots.txt).*)']
 }
